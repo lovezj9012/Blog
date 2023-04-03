7d2f459b6b62157c6d7523fd1ce0a2c06c849555
lovezj9012 <843265303@qq.com>
2023-04-03 08:40:01 +0800

    修改配置，删除无用代码

---
+++ Blog.JWT/Utils/ApiResult/ApiResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.JWT.Utils.ApiResult
+{
+    public class ApiResult
+    {
+        public int Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public int Total { get; set; }
+
+        public dynamic Data { get; set; }
+    }
+}
